dacdd405177ee913915ed79b03fe747c2301f12d
1JungleK <aczhixinli@outlook.com>
2025-05-10 23:36:40 -0400

    add MessageDao interface and Message model

---
+++ src/main/java/com/jungle/database/dao/MessageDao.java
@@ -0,0 +1,25 @@
+package com.jungle.database.dao;
+
+import com.jungle.database.model.Message;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *  The interface of message-dao
+ * 
+ *  @author Zhixin Li
+ * 
+ *  @version May 10, 2025
+ */
+
+public interface MessageDao {
+
+    boolean createMessage(int senderId, int receiverId, String content) throws SQLException;
+
+    List<Message> getMessagesByUserId(int userId) throws SQLException;
+
+    List<Message> getMessagesBySenderAndReceiver(int senderId, int receiverId) throws SQLException;
+
+    boolean deleteMessage(int messageId) throws SQLException;
+
+}
